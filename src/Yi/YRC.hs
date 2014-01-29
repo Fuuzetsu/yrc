28b5224053292e0ba1b80a0a466cd96e008ea767
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-01-29 17:15:13 +0000

    Pull in some deps

---
+++ src/Yi/YRC.hs
@@ -0,0 +1,10 @@
+-- |
+-- Module      :  Yi.YRC
+-- Copyright   :  (c) Mateusz Kowalczyk 2013
+-- License     :  GPL-3
+--
+-- Maintainer  :  fuuzetsu@fuuzetsu.co.uk
+-- Stability   :  experimental
+--
+-- Entry point to and the heard of YRC
+module Yi.YRC where
